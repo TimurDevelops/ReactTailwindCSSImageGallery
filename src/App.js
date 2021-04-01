cb6b72e994cb2f9ee03db1b38b5bdfa53a21c975
TimurDevelops <timur.eremchenko@yandex.ru>
2021-04-01 23:47:16 +0300

    Gallery Working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ImageCard from "./components/ImageCard";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(
+        /**
+         * @param {{hits:string}} data
+         */
+        data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
   return (
-    <div>
-      <h1 className="text-6xl">Hello World</h1>
+    <div className="container mx-auto">
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+          {images.map(image => (
+            <ImageCard key={image.id} image={image}/>
+          ))}
+        </div>}
     </div>
   );
 }
---
+++ src/components/ImageCard.js
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const ImageCard = ({image}) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src={image.webformatURL} alt="" className="w-full"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by {image.author}
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            {image.views}
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            {image.downloads}
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            {image.likes}
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        {image.tags.split(', ').forEach(tag => (
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            {tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+ImageCard.propTypes = {
+  image: PropTypes.shape({
+    tags: PropTypes.string,
+    views: PropTypes.number,
+    downloads: PropTypes.number,
+    likes: PropTypes.number,
+    webformatURL: PropTypes.string,
+    author: PropTypes.string
+  }),
+}
+
+export default ImageCard;
